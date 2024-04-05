9e860130f936f1193ac1a96ccf73058b475dab6b
Danish <muhammeddanishkv05@gmail.com>
2024-04-05 14:44:28 +0530

    DSA

---
+++ Binary search.py
@@ -0,0 +1,19 @@
+def binary(arr1, find):
+    l = 0
+    r = len(arr1)
+
+    while l <= r:
+        m = (l + r) // 2
+
+        if arr1[m] == find:
+            return m
+        elif find < arr1[m]:
+            r = m - 1
+        else:
+            l = m + 1
+    return -1
+
+
+arr = [11, 12, 21, 23, 32, 34, 45, 56, 67, 72, 87, 90]
+k = 12
+p = binary(arr, k)

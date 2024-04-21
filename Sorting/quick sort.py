9368cd34a733f32165ff9cf39b57c1a47b65eb9c
Danish <muhammeddanishkv05@gmail.com>
2024-04-21 10:56:58 +0530

    quick sort and selection sort done

--- Sorting/quick sort.py
+++ Sorting/quick sort.py
@@ -1,30 +1,30 @@
-def pivot_index(array, first, last):
-    pivot = array[first]
+def pivot_index(arr, first, last):
+    pivot = arr[first]
     left = first + 1
     right = last
 
     while True:
-        while left <= right and array[left] <= pivot:
+        while left <= right and arr[left] <= pivot:
             left += 1
-        while left <= right and array[right] >= pivot:
+        while left <= right and arr[right] >= pivot:
             right -= 1
         if left > right:
             break
         else:
-            array[left], array[right] = array[right], array[left]
-    array[first], array[right] = array[right], array[first]
+            arr[left], arr[right] = arr[right], arr[left]
+    arr[first], arr[right] = arr[right], arr[first]
 
     return right
 
 
-def quicksort(array, first, last):
+def quicksort(arr, first, last):
     if first < last:
-        p = pivot_index(array, first, last)
-        quicksort(array, first, p - 1)
-        quicksort(array, p + 1, last)
+        p = pivot_index(arr, first, last)
+        quicksort(arr, first, p - 1)
+        quicksort(arr, p + 1, last)
 
 
-array = [20, 11, 2, 5, 9]
+array = [64, 34, 25, 12, 22, 11, 90]
 print(array)
 quicksort(array, 0, len(array) - 1)
 print(array)
--- Sorting/selection sort.py
+++ Sorting/selection sort.py
@@ -1,19 +1,19 @@
-arr = [1, 20, 2, 90, 23, 1, 5, 67, 21]
+arr = [64, 34, 25, 12, 22, 11, 90]
 
 for i in range(len(arr)):
-    min = i
+    val_min = i
     for j in range(i + 1, len(arr)):
-        if arr[j] < arr[min]:
-            min = j
-    arr[i], arr[min] = arr[min], arr[i]
+        if arr[j] < arr[val_min]:
+            val_min = j
+    arr[i], arr[val_min] = arr[val_min], arr[i]
 
 print(arr)
 
 
+# Using Inbuild method min()
 
-# Using min()
-# for i in range(len(arr)):
-#     min_val = min(arr[i:])
-#     min_index = arr.index(min_val,i)
-#     arr[i], arr[min_index] = arr[min_index], arr[i]
-# print(arr)
+for i in range(len(arr)):
+    min_val = min(arr[i:])
+    min_index = arr.index(min_val, i)
+    arr[i], arr[min_index] = arr[min_index], arr[i]
+print(arr)
